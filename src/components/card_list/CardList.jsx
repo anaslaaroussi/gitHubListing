841bd0cf2e864c6f0e44775ae18831a0aa0f01c1
Anas Laaroussi Alami <anaslaaroussialami@gmail.com>
2019-05-17 22:24:06 +0000

    fix the null value exception in the github fetch api

--- src/components/card_list/CardList.jsx
+++ src/components/card_list/CardList.jsx
@@ -6,29 +6,31 @@ export const CardList = ({ repos }) => {
   return (
     <div style={{ transform: "translateY(70px)", zIndex: 2 }}>
       {repos.map((repo, index) => {
-        const {
-          owner,
-          description,
-          stargazers_count,
-          open_issues_count,
-          created_at,
-          full_name
-        } = repo;
+        if (repo) {
+          const {
+            owner,
+            description,
+            stargazers_count,
+            open_issues_count,
+            created_at,
+            full_name
+          } = repo;
 
-        var bgColor = index % 2 ? "#F8F8F8" : "#fff";
-        return (
-          <Card
-            key={index}
-            picture={owner.avatar_url}
-            profileName={owner.login}
-            description={description}
-            stars={stargazers_count}
-            issues={open_issues_count}
-            createdAt={created_at}
-            repositoryName={full_name}
-            backgroundColor={bgColor}
-          />
-        );
+          var bgColor = index % 2 ? "#F8F8F8" : "#fff";
+          return (
+            <Card
+              key={index}
+              picture={owner.avatar_url}
+              profileName={owner.login}
+              description={description}
+              stars={stargazers_count}
+              issues={open_issues_count}
+              createdAt={created_at}
+              repositoryName={full_name}
+              backgroundColor={bgColor}
+            />
+          );
+        }
       })}
     </div>
   );
