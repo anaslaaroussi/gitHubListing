5545418169aa4b402a570f79723277c304a32a3a
Anas Laaroussi Alami <anaslaaroussialami@gmail.com>
2019-05-18 12:33:35 +0000

    fix test with redux in App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { http_reducer } from "./redux/reducer";
+import rootSaga from "./redux/saga";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+// run the saga
+
+it("renders without crashing", () => {
+  // create the saga middleware
+
+  const sagaMiddleware = createSagaMiddleware();
+
+  let store = createStore(
+    http_reducer,
+    compose(applyMiddleware(sagaMiddleware))
+  );
+  sagaMiddleware.run(rootSaga);
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
